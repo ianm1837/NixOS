7e5467e580ab60aebd4693f0e881846acfd4f8b4
ianm1837 <ian@iwm.dev>
2024-02-04 19:57:13 -0800

    move kanshi to nix from dot

--- components/home-manager-users/ianm1837/home.nix
+++ components/home-manager-users/ianm1837/home.nix
@@ -20,6 +20,63 @@ in
   };
 
   services = {
+    kanshi = {
+      enable = true;
+      profiles = {
+        laptop-only = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+              scale = 1.6;
+              mode = "2560x1600@165";
+            }
+          ];
+        };
+        one-monitor-a = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "DP-1";
+              status = "enable";
+            }
+          ];
+        };
+        one-monitor-b = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "DP-2";
+              status = "enable";
+            }
+          ];
+        };
+        both-monitors = {
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "GIGA-BYTE TECHNOLOGY CO., LTD. Gigabyte M32Q 0x00000583";
+              status = "enable";
+              position = "0,0";
+            }
+            {
+              criteria = "GIGA-BYTE TECHNOLOGY CO., LTD. Gigabyte M32Q 0x00000228";
+              status = "enable";
+              position = "2560,0";
+            }
+          ];
+        };
+      };
+    };
     dunst = {
       enable = true;
       settings = {
@@ -205,10 +262,10 @@ in
       #   source = ./raw-dots/dunst;
       #   recursive = true;
       # };
-      ".config/kanshi" = {
-        source = ./raw-dots/kanshi;
-        recursive = true;
-      };
+      # ".config/kanshi" = {
+      #   source = ./raw-dots/kanshi;
+      #   recursive = true;
+      # };
       ".config/kitty" = {
         source = ./raw-dots/kitty;
         recursive = true;
