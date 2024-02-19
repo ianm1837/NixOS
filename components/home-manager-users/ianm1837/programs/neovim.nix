2b459b23b7294804ecf90a80fb1808d4f15b7257
ianm1837 <ian@iwm.dev>
2024-02-18 19:41:13 -0800

    add lspconfig

--- components/home-manager-users/ianm1837/programs/neovim.nix
+++ components/home-manager-users/ianm1837/programs/neovim.nix
@@ -12,6 +12,16 @@
     plugins = with pkgs.vimPlugins; [
       nvim-web-devicons
       {
+        plugin = nvim-lspconfig;
+        type = "lua";
+        config = ''
+          local lspconfig = require('lspconfig')
+          lspconfig.pyright.setup {}
+          lspconfig.tsserver.setup {}
+          lspconfig.nixd.setup{}
+        '';
+      }
+      {
         plugin = nvim-tree-lua;
         type = "lua";
         config = ''
@@ -27,6 +37,13 @@
       }
       {
         plugin = vim-tmux-navigator;
+      }
+      {
+        plugin = commentary;
+        type = "lua";
+        config = ''
+          
+        '';
       }
       {
         plugin = catppuccin-nvim;
@@ -61,11 +78,52 @@
     ];
 
     extraLuaConfig = ''
-      -- theme
+      -- filetype plugin indent on
+      vim.cmd('filetype plugin indent on')
+      vim.cmd([[
+          autocmd FileType apache setlocal commentstring=#\ %s
+      ]])
 
+      --lspconfig settings 
 
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      vim.keymap.set('n', '<space>l', vim.diagnostic.open_float)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
-      -- Basic settings
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<space>lf', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
+      })
+
+     -- Basic settings
       vim.o.relativenumber = true        -- Show line numbers
       vim.o.syntax = "on"        -- Enable syntax highlighting
       vim.wo.wrap = false        -- Disable text wrapping
